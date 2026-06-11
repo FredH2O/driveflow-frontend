d59f89e44addcabc75690c6b02e3ffc583e0c763
Fred <huertaserick@yahoo.ie>
2026-06-11 19:59:48 +0100

    Driveflow, added styling, purple/500 theme, japanese garage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import background from "../assets/driveflow-bg.jpg";
 
+const buttons = ["Book a Service", "Our Story"];
+
 function Hero() {
   return (
     <section className="relative flex flex-col justify-center h-screen">
@@ -8,10 +10,28 @@ function Hero() {
         src={background}
         alt="Driveflow's Background"
       />
-      <div className="absolute bg-black/50 inset-0"></div>
-      <h1 className="relative z-10 text-7xl font-bold italic text-center drop-shadow-lg">
-        Driveflow
-      </h1>
+      <div className="absolute inset-0 bg-linear-to-t from-black/90 via-black/70 to-transparent"></div>
+      <div className="relative flex flex-col gap-3 z-10 text-center">
+        <h1 className="text-7xl font-bold italic text-center drop-shadow-lg">
+          Drive<span className="text-purple-500">flow</span>
+        </h1>
+
+        <div>
+          <p>ダブリン初の日本人向けガレージ</p>
+          <p className="italic">
+            Dublin's Elite Garage Precision care for your vehicle, rooted in
+            Japanese craftsmanship.
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-center gap-4">
+          {buttons.map((title) => (
+            <button className="border p-3 rounded-full" key={title}>
+              {title}
+            </button>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
