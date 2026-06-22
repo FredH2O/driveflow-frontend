7595c81d60d411f1005835ec011750c6bd96bcd0
Fred <huertaserick@yahoo.ie>
2026-06-22 19:50:48 +0100

    style: updated card for testimonial

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,11 +1,13 @@
 function TestimonialCard({ name, comment, image }) {
   return (
-    <div className="bg-white border border-gray-100 rounded-xl p-6 shadow-sm hover:shadow-md transition-shadow duration-300">
-      <p className="text-gray-600 italic leading-relaxed mb-4">“{comment}”</p>
+    <div className="bg-white flex flex-col h-full border border-gray-100 rounded-xl p-6 shadow-sm hover:shadow-md hover:-translate-y-1 transition-all ease-in-out hover:bg-purple-200 duration-300">
+      <p className="text-gray-600 grow italic leading-relaxed mb-4">
+        “{comment}”
+      </p>
 
-      <div>
+      <div className="">
         <img
-          className="w-20 h-20 border rounded-full bg-purple-500 object-cover"
+          className="w-20 h-20 border rounded-full bg-purple-700 object-cover"
           src={image}
           alt={name}
         />
